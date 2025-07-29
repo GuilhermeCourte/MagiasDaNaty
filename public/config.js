25c7be3a28188416b4f627af87ae4683213d0979
GuilhermeCourte <guicourte0@gmail.com>
2025-07-29 01:38:47 -0300

    feat: Add Firebase Hosting GitHub Actions workflow

---
+++ public/config.js
@@ -0,0 +1,23 @@
+// config.js
+// Cole este código em um novo arquivo chamado config.js na mesma pasta do seu crud.html
+
+// Configuração do seu projeto Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyBn5ovTJVje3OH1B4Sgqip2QbU23eQRuD0",
+    authDomain: "magias-da-naty.firebaseapp.com",
+    projectId: "magias-da-naty",
+    storageBucket: "magias-da-naty.firebasestorage.app",
+    messagingSenderId: "168833550315",
+    appId: "1:168833550315:web:222d383066b3d71de0fbae",
+    measurementId: "G-7GDJHC0QPY"
+};
+
+// IMPORTANTE: Substitua a string abaixo pelo UID do seu usuário administrador do Firebase.
+// Para encontrar o UID: Vá em Firebase Console > Authentication > Users > copie o UID do seu usuário admin.
+export const ADMIN_UID = "BfYKKYt3YSPMJo1jZutzMstKyAQ2";
+
+// IMPORTANTE: Substitua a string abaixo pela sua CHAVE DE API obtida no site do imgbb.
+export const IMGBB_API_KEY = "f6c013097f8f1e0f6d5c4cf1bf68d0b5";
+
+
+
